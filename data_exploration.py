837528bf8e788fe4bcbe80552f0ded31cb4f3b8a
Yucheng Fang <664947387@qq.com>
2020-03-31 23:02:11 -0400

    update

--- data_exploration.py
+++ data_exploration.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 
 raw_data = pd.read_csv("covid_19_clean_complete.csv")
+raw_data["Date"] = raw_data["Date"].astype("datetime64")
 
 # confirmed, deaths recovered all equal to zero
 zeros_subset = raw_data.loc[np.logical_and(np.logical_and(
@@ -20,5 +21,10 @@ zeros_subset = raw_data.loc[np.logical_and(np.logical_and(
 # unique country
 unique_country = raw_data["Country/Region"].unique()
 
+# total examples by country
+groupBy_country = raw_data[
+    ["Country/Region", "Date", "Confirmed", "Deaths", "Recovered"]
+    ].groupby(["Country/Region", "Date"], as_index=False).sum()
 
-# total 
+# China 
+china_df = groupBy_country.loc[groupBy_country["Country/Region"] == "China", ]
